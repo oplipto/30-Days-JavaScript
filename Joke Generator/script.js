1581a1e5fa7da85dd4da56383d94d0188767fdb9
Rohit Kumar <104258989+oplipto@users.noreply.github.com>
2023-11-11 09:13:12 +0000

    My New Project Jokes Generator

---
+++ Joke Generator/script.js
@@ -0,0 +1,14 @@
+// Get the button and the paragraph elements
+var myButton = document.getElementById('myButton');
+var secondPara = document.getElementById('secondPara');
+
+// Add an event listener to the button
+myButton.addEventListener('click', function() {
+  // Fetch a random joke
+  fetch('https://official-joke-api.appspot.com/random_joke')
+    .then(response => response.json())
+    .then(data => {
+      // Set the text of the paragraph to the joke
+      secondPara.innerText = data.setup + ' ' + data.punchline;
+    });
+});
